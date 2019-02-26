59bf4adc670cf26a940d9ac358e6e3e824865a2b
Danny Martin <danielmartin0126@gmail.com>
2019-02-26 21:41:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(string)
   array = string.split() 
   reversed = []
   puts array
-  array.each do |word|
+  array.collect do |word|
      reversed.push(word.reverse) 
   end
   reversed.join(" ")
